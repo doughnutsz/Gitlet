27087201f91c6b85a60d69a47b123f23db20aa7c
SeaSkysz <2448861628@qq.com>
2021-07-14 17:56:05 +0800

    raw

---
+++ gitlet/Repository.java
@@ -0,0 +1,42 @@
+package gitlet;
+
+import java.io.File;
+import java.io.IOException;
+
+import static gitlet.Utils.*;
+
+// TODO: any imports you need here
+
+/** Represents a gitlet repository.
+ *  TODO: It's a good idea to give a description here of what else this Class
+ *  does at a high level.
+ *
+ *  @author TODO
+ */
+public class Repository {
+    /**
+     * TODO: add instance variables here.
+     *
+     * List all instance variables of the Repository class here with a useful
+     * comment above them describing what that variable represents and how that
+     * variable is used. We've provided two examples for you.
+     */
+
+    /** The current working directory. */
+    public static final File CWD = new File(System.getProperty("user.dir"));
+    /** The .gitlet directory. */
+    public static final File GITLET_DIR = join(CWD, ".gitlet");
+
+    /* TODO: fill in the rest of this class. */
+    public static void setupPersistence() {
+        GITLET_DIR.mkdir();
+        File f = join(GITLET_DIR,"story");
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File g = join(GITLET_DIR,"dogs");
+        g.mkdir();
+    }
+}
